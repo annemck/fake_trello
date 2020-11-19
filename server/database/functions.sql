16934fa9d9fe3c28082ded77693626f51479bfe4
annemck <annekh88@gmail.com>
2020-11-19 17:32:32 +0000

    cascade delete tested as working. update on password hashed correctly.

--- server/database/functions.sql
+++ server/database/functions.sql
@@ -1,7 +1,11 @@
 CREATE OR REPLACE FUNCTION fn_hash_password()
-RETURNS trigger AS '
+RETURNS trigger AS $$
   BEGIN
-    new.user_password := crypt(new.user_password, gen_salt(''bf''));
-    RETURN NEW;
-  END'
+    IF length(new.user_password) < 8 THEN
+      RAISE EXCEPTION 'Password must be at least 8 characters.';
+    ELSE
+      new.user_password := crypt(new.user_password, gen_salt('bf'));
+      RETURN NEW;
+    END IF;
+  END $$
 LANGUAGE 'plpgsql';
