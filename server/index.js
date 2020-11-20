ac84f43da04d70628ccb51d99d77de071dca3110
annemck <annekh88@gmail.com>
2020-11-20 14:36:38 +0000

    can update user details

--- server/index.js
+++ server/index.js
@@ -11,11 +11,12 @@ app.use(express.json()); //req.body
 
 app.post("/user", async(req, res) => {
   try {
-    
-    console.log(req.body);
-    const body = req.body;
+
+    const details = req.body;
   
-    const newUser = await pool.query("INSERT INTO users(user_email, first_name, last_name, user_password) VALUES($1, $2, $3, $4) RETURNING user_id", [body.user_email, body.first_name, body.last_name, body.user_password]);
+    const newUser = await pool.query(
+      "INSERT INTO users(user_email, first_name, last_name, user_password) VALUES($1, $2, $3, $4) RETURNING user_id"
+      , [details.user_email, details.first_name, details.last_name, details.user_password]);
     
     res.json(newUser.rows);
     
@@ -29,7 +30,9 @@ app.get("/user/:id", async(req, res) => {
     
     const {id} = req.params;
     
-    const user = await pool.query("SELECT user_email, first_name, last_name FROM users WHERE user_id = $1", [id]);
+    const user = await pool.query(
+      "SELECT user_email, first_name, last_name FROM users WHERE user_id = $1"
+      , [id]);
     
     res.json(user.rows);
     
@@ -38,6 +41,23 @@ app.get("/user/:id", async(req, res) => {
   }
 });
 
+app.put("/user/:id", async(req, res) => {
+  try {
+    
+    const {id} = req.params;
+    const details = req.body;
+    
+    console.log(details);
+    const updateUserDetails = await pool.query(
+      "UPDATE users SET user_email = $1, first_name = $2, last_name = $3, user_password = $4 WHERE user_id = $5"
+      , [details.user_email, details.first_name, details.last_name, details.user_password, id]);
+    
+    res.json("User details updated");
+    
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 
 
