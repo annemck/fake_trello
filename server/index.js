15ef7b8aa2eef13971e0422d862cdf99661a596c
annemck <annekh88@gmail.com>
2020-11-22 17:04:43 +0000

    add project created/tested

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const users = require('./routes/users.js');
+const projects = require('./routes/projects.js');
+
+app.use(cors());
+app.use(express.json());
+
+app.set('port', process.env.PORT || 5000);
+
+
+//User CRUD action routes
+app.get('/user/:id', users.find);
+app.post('/user', users.create);
+app.put('/user/:id', users.update);
+app.delete('/user/:id', users.remove);
+
+
+//Project CRUD action routes
+// app.get('/project/:id', projects.find);
+app.post('/project', projects.create);
+// app.put('/project/:id', projects.update);
+// app.delete('/project/:id', projects.remove);
+
+app.listen(app.settings.port, function() {
+  console.log('Server is running on port 5000. Press CTRL+C to stop server')
+});
---
+++ server/routes/projects.js
@@ -0,0 +1,68 @@
+const pool = require("../database/db_pool.js");
+
+exports.create = async function(req, res) {
+  try {
+
+    const details = req.body;
+
+    const newProject = await pool.query(
+      `INSERT INTO projects(proj_name, proj_desc, user_id)
+      VALUES($1, $2, $3)
+      RETURNING proj_id`
+      , [details.proj_name, details.proj_desc, details.user_id]);
+      
+    console.log('post function running');
+    
+    res.json(newProject.rows);
+
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+// exports.find = async function(req, res) {
+//   try {
+//     const project_id = req.params.id;
+//
+//     const project = await pool.query(`SELECT proj_name, proj_desc
+//                           FROM projects
+//                           WHERE proj_id = $1`, [project_id]);
+//
+//     console.log("find function running");
+//
+//     res.json(project.rows);
+//
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// }
+//
+// exports.update = async function(req, res) {
+//     try {
+//
+//       const {id} = req.params;
+//       const details = req.body;
+//
+//       const editedProject = await pool.query(`UPDATE projects SET proj_name = $1,
+//                                             proj_desc = $2,
+//                                             WHERE proj_id = $3`
+//         , [details.proj_name, details.proj_desc, id]);
+//
+//       res.json("Project details updated");
+//
+//     } catch (err) {
+//       console.log(err.message);
+//     }
+// }
+//
+// exports.remove = async function(req, res) {
+//   try {
+//     const {id} = req.params;
+//     const deleteProject = await pool.query(`DELETE FROM projects
+//                                         WHERE user_id = $1;`, [id]);
+//
+//     res.json("Project and associated tasks deleted");
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// }
